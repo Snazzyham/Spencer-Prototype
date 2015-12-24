0445b3b5062340676ccd385fb44a6a57deff98b9
Soham Adwani <sohamthenerd@gmail.com>
2015-12-24 15:34:00 +0700

    changed switch statements to array
    Previously, this program used switch statements. as time goes by, that
    may cause bloating, which is why I changed it to read strings from an
    array. Maybe in the future I will implement another example that uses
    Sets, and create a second program that allows you to add fortunes
    without having to modify the code.

--- Spencer.java
+++ Spencer.java
@@ -4,6 +4,16 @@ public class Spencer
 {
 	public static void main(String[] args)throws IOException
 	{
+		String[] fortunes =  {"Tonight, you may or may not have a really bad dream.",
+		"Sometimes when you're sad, something will come along to make you happy, but you will lose a loved one in the process.",
+		 "You will find Rp. 50,000 on the ground. You will use it all on chicken nuggets.",
+		 "Remember when you decided not to leave the party early? Had you left, you would've met the girl of your dreams.",
+		 "Your favourite TV character will be killed off, but hey, oreos are on discount.",
+		 "Life is hard, but you will be too, because that Panadol you took was actually Viagra.",
+		 "Shit happens, but mostly to you.",
+		 "You will win an award, however, it will be \"most likely to fail at life\". ",
+		 "Your phone will break. You'll get a new one. That will break too. "
+		 };
 		Scanner kb = new Scanner(System.in);
 		System.out.println("Welcome to Spencer, would you like a fortune?");
 		int counter = 0;
@@ -14,31 +24,10 @@ public class Spencer
 		if (choice == 1)
 		{
 			Random rand = new Random();
-			int  n = rand.nextInt(9) + 1;
+			int  n = rand.nextInt(8) + 1;
+			String fortune = fortunes[n];
+			System.out.println(fortune);
 
-			String fortune;
-        switch (n) {
-            case 1:  fortune = "Tonight, you may or may not have a really bad dream.";
-                     break;
-            case 2:  fortune = "Sometimes when you're sad, something will come along to make you happy, but you will lose a loved one in the process.";
-                     break;
-            case 3:  fortune = "You will find Rp. 50,000 on the ground. You will use it all on chicken nuggets.";
-                     break;
-            case 4:  fortune = "Remember when you decided not to leave the party early? Had you left, you would've met the girl of your dreams.";
-                     break;
-            case 5:  fortune = "Your favourite TV character will be killed off, but hey, oreos are on discount.";
-                     break;
-            case 6:  fortune = "Life is hard, but you will be too, because that Panadol you took was actually Viagra.";
-                     break;
-            case 7:  fortune = "Shit happens, but mostly to you.";
-                     break;
-            case 8:  fortune = "You will win an award, however, it will be \"most likely to fail at life\". ";
-                     break;
-            default: fortune = "Your phone will break. You'll get a new one. That will break too. ";
-                     break;
-        }
-        System.out.println(fortune);
-			
 		}
 		if (choice == 3)
 		{
